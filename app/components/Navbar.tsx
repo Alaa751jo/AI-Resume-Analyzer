a580292cdb647027b38a3fdc5a2fd9b102d2b2b3
Ratna-Babu <ratna71babu.com>
2025-07-23 12:05:58 +0530

    upload

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,10 @@
-import {Link} from "react-router"
+import {Link} from "react-router";
 
-const Navbar:() => Element = () => {
+const Navbar = () => {
     return (
         <nav className="navbar">
             <Link to="/">
-                <p className="text-2xl font-bold text-gradient">Resume</p>
+                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
             </Link>
             <Link to="/upload" className="primary-button w-fit">
                 Upload Resume
